b3b512127fc99b9651fe1e860aa71d04c5384e48
Luchigarcia <albalucia-2009@hotmail.com>
2023-07-26 17:07:23 -0500

    añadido datos

---
+++ Practicas javascript/01 fundamentos/app.js
@@ -0,0 +1,28 @@
+// console.log('Hola Mundo');
+// console.warn('esta es una advertencia');
+// console.error('Esto es un error');
+
+
+let a = 10,
+    b = 20,
+    c ='Hola ',
+    d = 'Thor',
+    saludo = c+d;
+
+const z = 30;
+
+let x = a+c;
+// console.log('x =', x);
+// console.log('a =', a);
+// console.log('b =', b);
+
+console.log('%c Mis variables', 'color: blue; font-weight:bold');
+console.log({x});
+console.log({a});
+console.log({saludo});
+console.table({a, b, c, d, saludo});
+
+alert('Hola mundo');
+prompt('cuál es tu nombre?');
+let nombre = prompt('cuál es tu nombre?', 'Angelica');
+
